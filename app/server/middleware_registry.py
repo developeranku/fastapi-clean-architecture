97f829d53302c52b1a01f62d6a11ed2db5fe5506
Ankur Sharma <ankur4736@gmail.com>
2025-05-08 18:50:10 +0530

    Update middleware_registry.py

--- app/server/middleware_registry.py
+++ app/server/middleware_registry.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.server.middlewares.request_logger import RequestLoggingMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from app.config.settings import ALLOWED_ORIGINS
-from app.server.response_handler import api_exception_handler
+from app.server.response_handler import api_exception_handler, unhandled_exception_handler
 from app.server.exceptions import ApiException
 
 
@@ -16,3 +16,4 @@ def register_middlewares(app: FastAPI):
     )
     app.add_middleware(RequestLoggingMiddleware)
     app.add_exception_handler(ApiException, api_exception_handler)
+    app.add_exception_handler(Exception, unhandled_exception_handler)
